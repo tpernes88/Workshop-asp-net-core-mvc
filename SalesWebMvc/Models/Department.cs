92c31ea2fd3826e3a8d34e6bbfcda5cdb3bf7632
Tiago Pernes <pernes.9@emea.teleperformance.com>
2019-11-19 15:35:55 +0000

    CRUD Scaffolding

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,9 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
